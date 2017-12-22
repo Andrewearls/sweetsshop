6a85d1c29938efe0d0663069020b14ff4fdf7011
Andrew Earls <andrewearls@rocketmail.com>
2017-12-21 16:09:38 -0800

    fixed the nav bar and the menu title

---
+++ wp-content/themes/twentyseventeen-child/template-parts/navigation/navigation-top.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Displays top navigation
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.2
+ */
+
+?>
+<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Top Menu', 'twentyseventeen' ); ?>">
+	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false">
+		<?php
+		echo twentyseventeen_get_svg( array( 'icon' => 'bars' ) );
+		echo twentyseventeen_get_svg( array( 'icon' => 'close' ) );
+		_e( 'Menu', 'twentyseventeen' );
+		?>
+	</button>
+
+	<ul id="top-menu" class="menu">
+		<li class="menu-item"><a href="http://localhost/sweetshop/">Home</a></li>
+		<li class="menu-item"><a href="http://localhost/sweetshop/menu/">Menu</a></li>
+		<li class="menu-item"><a href="http://localhost/sweetshop/about/">About</a></li>
+		<li class="menu-item"><a href="http://localhost/sweetshop/contact/">Contact</a></li>
+	</ul>
+	
+	<?php
+	/**
+	--- Original Nav menu ---
+		wp_nav_menu( array(
+			'theme_location' => 'top',
+			'menu_id'        => 'top-menu',
+		) ); 
+	*/
+	?>
+
+
+	<?php if ( ( twentyseventeen_is_frontpage() || ( is_home() && is_front_page() ) ) && has_custom_header() ) : ?>
+		<a href="#content" class="menu-scroll-down"><?php echo twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'twentyseventeen' ); ?></span></a>
+	<?php endif; ?>
+</nav><!-- #site-navigation -->
